b446c5a7c6dd5f63fc708f28adc27ab2da80d801
unknown <dan_kavoulakos@hotmail.com>
2022-01-06 15:41:51 +0200

    - Add show scales func to init

--- img2midi/__init__.py
+++ img2midi/__init__.py
@@ -1,8 +1,9 @@
 # -*- pretty cool init mate? -*-
 
 import sys
-from img2midi.img2midi import ImgToMidi
+from img2midi.img2midi import ImgToMidi, show_scales
 
 __all__ = [
     'ImgToMidi',
+    'show_scales',
     ]
